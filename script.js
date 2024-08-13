a0cd0e22a5a5dd187d7899029aab1f14cfbf9f8d
joc105 <joc105@aber.ac.uk>
2024-08-13 15:01:09 +0100

    changes to script

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const email = "joshcant0477@gmail.com"
 
+document.getElementById("emailButton").addEventListener("click", function(){
+    document.getElementById("copyMessage").style.display = "block";
+
+    navigator.clipboard.writeText(email);
+
+    setTimeout(() => {
+        document.getElementById("copyTooltip").style.display = "none";
+    }, 1500)
+});
+    
