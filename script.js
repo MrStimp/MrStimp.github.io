5e500561a1bd0c193cc1107e90b8c19a7e66253f
joc105 <joc105@aber.ac.uk>
2024-08-13 15:18:18 +0100

    changes to home page

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const email = "joshcant0477@gmail.com"
 
 document.getElementById("emailButton").addEventListener("click", function(){
-    document.getElementById("copyMessage").style.display = "block";
+    document.getElementById("emailSvg").style.display = "none";
+    document.getElementById("tickSvg").style.display = "inline-block";
 
     navigator.clipboard.writeText(email);
 
     setTimeout(() => {
-        document.getElementById("copyMessage").style.display = "none";
+        document.getElementById("emailSvg").style.display = "inline-block";
+        document.getElementById("tickSvg").style.display = "none";
     }, 1500)
 });
     
